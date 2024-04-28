a8ba31de359216e735843e22bbe4b71d4edc70fc
anilbhangay <anilbhangay@gmail.com>
2024-04-29 00:00:48 +0530

    Added style css to legal ai page

--- src/components/Slider/Legal/legal.jsx
+++ src/components/Slider/Legal/legal.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import './legal.css';
+import React from "react";
+import "./legal.css";
 const Legal = () => {
   return (
-    <div className='container-legal'>
-    <div className='container-main-legal'>
-      <div className="head-top-legal">
-        <h1>LEGAL AI</h1>
-      </div>
-      <div className='container-boxs-legal'>
-        <div className='box-legal'></div>
+    <div className="container-legal">
+      <div className="container-main-legal">
+        <div className="head-top-legal">
+          <h5>Coming Soon</h5>
+        </div>
+        <div className="box-legal">
+          <div className="left-box"></div>
+          <div className="right-box"></div>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default Legal;
